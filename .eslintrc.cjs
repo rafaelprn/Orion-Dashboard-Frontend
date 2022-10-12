03ac28658c79efb8f4fb8b46f26728a101fc1b7e
Rafael Perina <rafael.prn3@gmail.com>
2022-10-12 00:21:24 -0300

    feat: initial configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'prettier'
+	],
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'react/react-in-jsx-scope':'off',
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
