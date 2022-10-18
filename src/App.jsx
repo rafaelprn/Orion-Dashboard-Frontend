5ddbc0ad557767479c69bce9abaa952697875785
Rafael Perina <rafael.prn3@gmail.com>
2022-10-17 21:00:38 -0300

    feat: add img charts and map leaftlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 import Card from './components/Card'
+import Image1 from './assets/Media_por_ano.png'
+import Image2 from './assets/Media_nos_ultimos_meses.png'
+import Image3 from './assets/Media_Estados.png'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './app.css'
 
 const App = () => {
+
+	const position = [51.505, -0.09]
+
 	return (
 		<div className="App">
-			<Card classGrid="div1" text="Por Ano"/>
-			<Card classGrid="div2" text="Meses do Último Ano"/>
-			<Card classGrid="div3" text="Total"/>
-			<Card classGrid="div4" text="Mapa"/>
+			<Card classGrid="div1" text="Por Ano">
+				<img src={Image1} alt="" />
+			</Card>
+
+			<Card classGrid="div2" text="Meses do Último Ano">
+				<img src={Image2} alt="" />
+			</Card>
+
+			<Card classGrid="div3" text="Total">
+				<img src={Image3} alt="" />
+			</Card>
+			 
+			<Card classGrid="div4" text="Mapa">
+				<MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					<Marker position={position}>
+						<Popup>
+							A pretty CSS3 popup. <br /> Easily customizable.
+						</Popup>
+					</Marker>
+				</MapContainer>
+			</Card>
 		</div>
 	)
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,18 +3,19 @@ import './styles.css'
 
 
 
-const Card = ({ classGrid, text }) => {
+const Card = ({ classGrid, text, children }) => {
 	return (
 		<article className={`card__container ${classGrid}`}>
 			<h2 className='card__title'>{text}</h2>
-			<div className='card__data'></div>
+			<div className='card__data'>{children}</div>
 		</article>
 	)
 }
 
 Card.propTypes = {
 	classGrid: PropTypes.string,
-	text: PropTypes.string
+	text: PropTypes.string,
+	children: PropTypes.node
 }
 
 export default Card
